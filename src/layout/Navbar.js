a74e9c2df05db680fefc8eefa5d469188391be99
kito078 <101482292+kito078@users.noreply.github.com>
2023-07-06 23:25:39 +0200

    props

---
+++ src/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import ProtoType from "prop-types";
+
+function Navbar({ title }) {
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub className="inline pr-2 text-3xl" />
+          {title}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+
+Navbar.protoType = {
+  title: ProtoType.string,
+};
+
+export default Navbar;
