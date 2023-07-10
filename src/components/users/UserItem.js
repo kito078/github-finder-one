82bcfb5e643726e91709087bef717e0eb25c9115
kito078 <101482292+kito078@users.noreply.github.com>
2023-07-10 14:00:06 +0200

    done user

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,7 +2,22 @@ import React from "react";
 
 function UserItem({ user: { login, avatar_url } }) {
   //console.log(user);
-  return <div className="card shadow-md compact side bg-base-100">{login}</div>;
+  return (
+    <div className="card shadow-md compact side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{login}</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UserItem;
